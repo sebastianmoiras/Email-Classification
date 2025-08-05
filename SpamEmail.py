d172a5a2b3a31a959e7ddb7073ed581106d2fe6d
sebastianmoiras <Jonathansebastian858@gmail.com>
2025-08-05 23:10:27 +0700

    Add files via upload

---
+++ SpamEmail.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+import numpy as np
+
+# Load model
+model = tf.keras.models.load_model("text_model.h5", custom_objects={'KerasLayer': hub.KerasLayer})
+
+# UI
+st.title("📩 Email Classification (SPAM OR NOT SPAM)")
+user_input = st.text_area("✍️ Enter the email content below:")
+
+if st.button("🔍 Predict"):
+    if not user_input.strip():
+        st.warning("⚠️ Text cannot be empty.")
+    else:
+        prob = float(model.predict([user_input])[0])  # hasil antara 0–1
+
+        if prob >= 0.5:
+            label = "SPAM"
+            confidence = prob
+            label_color = "🔴"
+        else:
+            label = "NOT SPAM"
+            confidence = 1 - prob
+            label_color = "🟢"
+
+        st.markdown(f"### ✅ Prediction: `{label_color} {label}`")
+        st.markdown(f"🧠 Confidence Level: `{confidence:.2%}`")
+        st.markdown(f"📊 Raw Probability (SPAM class): `{prob:.2f}`")
